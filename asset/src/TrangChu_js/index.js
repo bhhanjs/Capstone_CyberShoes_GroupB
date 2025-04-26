ed7339b2e935385f1bf34ec2ddea73ce5040b366
ThanhDanh <dtvgaming214@gmail.com>
2025-04-26 13:42:26 +0700

    update api product paging at carousel on file index.html

--- asset/src/TrangChu_js/index.js
+++ asset/src/TrangChu_js/index.js
@@ -42,5 +42,57 @@ productPromise
     });
   })
   .catch((err) => {
-    console.log("Can't get api: ", alert(err.message));
+    console.log("Can't get api: ", err.message);
+  });
+//-----------API paging---------------
+
+let slider = document.querySelector("#slider");
+let productPromise2 = axios({
+  url: "https://shop.cyberlearn.vn/api/Product/getpaging?pageIndex=1&pageSize=10&keywords=nike",
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+productPromise2
+  .then((response) => {
+    const products = response.data.content.items;
+    console.log("ProductPaging", products);
+    let content = "";
+    products.forEach((product, index) => {
+      const fullImageUrl = `https://shop.cyberlearn.vn/images/${product.image}`;
+      content += `
+          <div class="card">
+           <span class="tag">${product.name}</span>
+              <button><i class="fa-solid fa-cart-plus"></i></button>
+              <img src="${fullImageUrl}" alt="${product.name}" />
+              <h3>${product.alias}</h3>
+              <div class="price">${product.price} đ</div>
+              <p>${product.shortDescription}</p> 
+              <small>Size: ${product.size}</small>
+          </div>
+        `;
+      // Gán HTML xong mới khởi tạo Slick
+      slider.innerHTML = content;
+    });
+    setTimeout(() => {
+      $(".slider")
+        .not(".slick-initialized")
+        .slick({
+          slidesToShow: 5,
+          arrows: true,
+          dots: true,
+          infinite: false,
+          prevArrow: `<button type="button" class="slick-prev"><i class="fa-solid fa-arrow-left"></i></button>`,
+          nextArrow: `<button type="button" class="slick-next"><i class="fa-solid fa-arrow-right"></i></button>`,
+          responsive: [
+            { breakpoint: 1024, settings: { slidesToShow: 2 } },
+            { breakpoint: 768, settings: { slidesToShow: 1 } },
+          ],
+        });
+    }, 50);
+  })
+  .catch((error) => {
+    console.error("Can't get API:", error.message);
   });
